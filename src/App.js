eeeb700f55bb47d8c5a2d75e0351179a640b8480
Rob Jenkins <rwjenkins@gmail.com>
2023-02-02 11:01:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import SquaresBG from "react-animated-squares";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <SquaresBG speed={0.9} backgroundColor="#333333" />
+        <div className='namePlate'>
+          <div className='nameDiv'>
+            <h2>Rob Jenkins</h2>
+            <p>JavaScript Developer</p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
