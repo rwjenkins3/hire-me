0ba5e94dd9175a4d3ea8b588c44ed14824a4f3b1
Rob Jenkins <rwjenkins@gmail.com>
2023-02-14 11:05:48 -0500

    Working slides

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import SquaresBG from "react-animated-squares";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/css';
 
+import { Slide } from './Slide';
+
+
+SwiperCore.use([Autoplay]);
 
 function App() {
   return (
+
     <div className="App">
       <div className="top">
         <SquaresBG speed={0.9} backgroundColor="#333333" />
@@ -13,6 +21,53 @@ function App() {
             <p>JavaScript Developer</p>
           </div>
         </div>
+
+      </div>
+
+    
+      <div className="experience">
+        <Swiper loop={true} 
+                autoplay={{
+                    delay: 7000,
+                    disableOnInteraction: false
+                }}
+        >
+          <SwiperSlide>
+            <div className='slideRow'>
+              <div className='slide'>
+                <img src='slide1.jpg' alt='related visual' />
+              </div>
+              <div className='textBox'>
+                <h2>Skill 1</h2>
+                <p>A paragraph or two talking about it.</p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slideRow'>
+              <div className='slide'>
+                <img src='slide2.jpg' alt='related visual' />
+              </div>
+              <div className='textBox'>
+                <h2>Skill 2</h2>
+                <p>Details of skill 2</p>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slideRow'>
+              <div className='slide'>
+                <img src='slide3.jpg' alt='related visual' />
+              </div>
+              <div className='textBox'>
+                <h2>Skill 3</h2>
+                <p>Details of skill 2</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+
+  
 
       </div>
 
---
+++ src/Slide.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Slide = () => {
+  return (
+  <div className='slideRow'>
+    <div className='slide'>
+      <img src='slide1.jpg' alt='related visual' />
+    </div>
+    <div className='textBox'>
+      <h2>Skill 1</h2>
+      <p>A paragraph or two talking about it.</p>
+      <h2>Skill 2</h2>
+      <p>Details of skill 2</p>
+    </div>
+  </div>
+)
+}
